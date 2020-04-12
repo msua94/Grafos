ec462a9c1df417c9d5f93a7e1b568e7ec74ac3cc
Martín Suárez <martin.suarez.campos@gmail.com>
2020-04-11 18:15:24 -0600

    se termina el método para calcular la distancia más corta

--- Grafos/Grafos/Program.cs
+++ Grafos/Grafos/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Entidades;
+using Logica;
+using System;
+using System.Collections.Generic;
 
 namespace Grafos
 {
@@ -6,7 +9,32 @@ namespace Grafos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Vertice verticeA = new Vertice("A");
+            Vertice verticeB = new Vertice("B");
+            Vertice verticeC = new Vertice("C");
+            Vertice verticeD = new Vertice("D");
+            Vertice verticeE = new Vertice("E");
+            Vertice verticeF = new Vertice("F");
+
+            List<UnionVertice> unionesDeVertices = new List<UnionVertice> {
+                new UnionVertice(verticeA, verticeD, 4),
+                new UnionVertice(verticeA, verticeC, 2),
+                new UnionVertice(verticeC, verticeB, 1),
+                new UnionVertice(verticeB, verticeE, 2),
+                new UnionVertice(verticeD, verticeE, 12),
+                new UnionVertice(verticeD, verticeF, 5),
+                new UnionVertice(verticeE, verticeF, 6)
+            };
+
+            BuscadorRuta buscador = new BuscadorRuta();
+
+            List<Vertice> vertices = buscador.BuscaLasRutasMenores(verticeD, unionesDeVertices);
+
+            foreach (Vertice vertice in vertices) {
+                Console.WriteLine($"Vertice {vertice.Valor} : {vertice.DistanciaAcumulada}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
--- Grafos/Logica/BuscadorRuta.cs
+++ Grafos/Logica/BuscadorRuta.cs
@@ -5,17 +5,17 @@ namespace Logica
 {
     public class BuscadorRuta
     {
-
+        private Vertice VerticeActual = null;
+        private List<Vertice> Adyacentes = null;
         private Queue<Vertice> ColaPendientes = new Queue<Vertice>();
         private List<UnionVertice> Uniones = null;
         private List<UnionVertice> UnionesAdyacentes = null;
-        private List<Vertice> VerticesCalculados = null;
-        private Vertice VerticeActual = null;
-        private List<Vertice> Adyacentes = null;
+        private List<Vertice> VerticesCalculados = null;        
 
         public List<Vertice> BuscaLasRutasMenores(Vertice verticeInicial, List<UnionVertice> uniones) {
             Uniones = uniones;
             VerticesCalculados = new List<Vertice>();
+            verticeInicial.EstaEnCola = true;
             ColaPendientes.Enqueue(verticeInicial);
             EjecutaColaDePendientes();
             return VerticesCalculados;
@@ -29,15 +29,14 @@ namespace Logica
                 if (union.ContieneElVertice(VerticeActual.Valor))
                 {
                     var adyacente = union.ObtieneVerticeAdyacente(VerticeActual.Valor);
-                    // No puede estar en cola de pendientes y no tiene que haber sido utilizado
-                    if ((!adyacente.EstaEnCola) && (!adyacente.YaSeUtilizo))
+                    // Solo los adyacentes que no han sido utilizados como verticeActual
+                    if (!adyacente.YaSeUtilizo)
                     {
                         Adyacentes.Add(adyacente);
                         UnionesAdyacentes.Add(union);
                     }
                 }
             }
-
         }
 
         private void CalculaDistanciaDijkstra(Vertice adyacente, int distanciaEntreVertices) {
@@ -70,17 +69,44 @@ namespace Logica
         private void AgregaLosAdyacentesEnColaPendientes()
         {
             foreach(Vertice adyacente in Adyacentes)
-            {
-                adyacente.EstaEnCola = true;
-                ColaPendientes.Enqueue(adyacente);
+            {   
+                //Si ya está en la cola no se agrega
+                if (!adyacente.EstaEnCola) {
+                    adyacente.EstaEnCola = true;
+                    ColaPendientes.Enqueue(adyacente);
+                }                
             }
         }
 
+        private Vertice ObtieneElPendienteMenor()
+        {
+            var cantidadPendientes = ColaPendientes.Count - 1;
+
+            var verticeMenor = ColaPendientes.Dequeue();
+
+            while (cantidadPendientes > 0)
+            {
+                var verticeActual = ColaPendientes.Dequeue();
+                if (verticeMenor.DistanciaAcumulada > verticeActual.DistanciaAcumulada)
+                {
+                    ColaPendientes.Enqueue(verticeMenor);
+                    verticeMenor = verticeActual;
+                }
+                else
+                {
+                    ColaPendientes.Enqueue(verticeActual);
+                }
+
+                cantidadPendientes -= 1;
+            }
+
+            return verticeMenor;
+        }
+
         private void EjecutaColaDePendientes() {
-
-            if (ColaPendientes.Count > 0) {
-
-                VerticeActual = ColaPendientes.Dequeue();
+            if (ColaPendientes.Count > 0)
+            {
+                VerticeActual = ObtieneElPendienteMenor();
                 BuscaLosAdyacentes();
                 ActualizaDistanciaDeAdyacentes();
                 AgregaLosAdyacentesEnColaPendientes();
@@ -88,8 +114,6 @@ namespace Logica
                 VerticesCalculados.Add(VerticeActual);
                 EjecutaColaDePendientes();
             }
-
-        }
-        
+        }        
     }
 }
